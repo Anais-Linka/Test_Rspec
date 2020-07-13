573872cefbb437c9bc42fe4baa54a21cf2dcb64d
Anais-Linka <Nananoushka@free.fr>
2020-07-13 23:32:26 +0200

    Commit Test Rspec

---
+++ lib/03_basics.rb
@@ -0,0 +1,22 @@
+
+def who_is_bigger(a, b, c)
+  if (a == nil || b == nil || c == nil)
+   return "nil detected"
+  elsif (a > b && a > c)
+    return "a is bigger"
+   elsif (b > a && b > c)
+    return "b is bigger"
+  else 
+    return "c is bigger"
+  end
+end
+
+def reverse_upcase_noLTA(str)
+    str.upcase.reverse.delete"LTA"
+end
+
+def find_42(a)
+  return a.include?(42) 
+end
+
+#?
